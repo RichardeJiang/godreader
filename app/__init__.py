429ddd62808f650d943de6a5a357fb7351090a59
RichardeJiang <richardejiang@hotmail.com>
2019-03-01 16:34:06 -0500

    added in db

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,14 @@
 from flask import Flask
-# from config import Config
+from config import Config
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_migrate import Migrate
 
 # Initialize the app
-app = Flask(__name__, instance_relative_config=True)
-
+app = Flask(__name__)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 # Load the views
-from app import views
+from app import views, models
 
 # Load the config file
-app.config.from_object('config')
+app.config.from_object(Config)
